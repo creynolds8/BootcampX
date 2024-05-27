3eba78dbb9fecb028a37a936c43828b98ed10ebc
creynolds8 <cameron8.cr@gmail.com>
2024-05-27 15:17:13 -0700

    update to match compass answer exactly

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,16 +1,16 @@
 CREATE TABLE assignments (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  content VARCHAR(255),
-  day DATE,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
   chapter INTEGER,
-  duration VARCHAR(255)
+  duration INTEGER
 );
 
 CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER NOT NULL REFERENCES assignments(id),
-  student_id INTEGER NOT NULL REFERENCES students(id),
-  duration VARCHAR(255),
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
   submission_date DATE
 );
