9a3838d357f5608e88635ff75b719a972dc0de62
creynolds8 <cameron8.cr@gmail.com>
2024-05-28 14:35:11 -0700

    query meets assignment requirements, my code

--- 4_queries/2_student_total_assistance_requests.sql
+++ 4_queries/2_student_total_assistance_requests.sql
@@ -1,5 +1,5 @@
-SELECT COUNT(assistance_requests.id) AS total_assistances, teachers.name AS name
+SELECT COUNT(assistance_requests.id) AS total_assistances, students.name AS name
 FROM assistance_requests
-JOIN teachers ON teachers.id = teacher_id
-WHERE teachers.name = 'Waylon Boehm'
-GROUP BY teachers.name;
+JOIN students ON students.id = student_id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
