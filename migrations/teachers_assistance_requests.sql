1acdbb70e9356df77210ab5d2afbd3a924fc5e61
creynolds8 <cameron8.cr@gmail.com>
2024-05-28 14:05:17 -0700

    minor adjustments to teachers table to match compass example

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -1,9 +1,9 @@
 CREATE TABLE teachers (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
+  name VARCHAR(255) NOT NULL
   start_date DATE,
   end_date DATE,
-  is_active BOOLEAN
+  is_active BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE assistance_requests (
