09ca54803cabcd2b7c14589d4091b2990d4e5c9a
creynolds8 <cameron8.cr@gmail.com>
2024-05-28 15:45:14 -0700

    query meets assignment requirements, my code

--- 4_queries/6_longest_cohort_assistance_time.sql
+++ 4_queries/6_longest_cohort_assistance_time.sql
@@ -3,4 +3,5 @@ FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
-ORDER BY average_assistance_time;
+ORDER BY average_assistance_time DESC
+LIMIT 1;
