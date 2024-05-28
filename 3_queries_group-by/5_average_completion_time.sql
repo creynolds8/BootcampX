e3da9d0fa1b919605eb69d9befd372c80e0e4a37
creynolds8 <cameron8.cr@gmail.com>
2024-05-27 18:55:39 -0700

    query meets activity requirements, my code

---
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -0,0 +1,6 @@
+SELECT students.name, AVG(assignment_submissions.duration) AS average_completion_time
+FROM assignment_submissions
+JOIN students ON students.id = assignment_submissions.student_id
+WHERE students.end_date IS NULL
+GROUP BY students.id
+ORDER BY average_completion_time DESC;
