c0082e2917ebe6f854c3efce8109671593fa2d48
creynolds8 <cameron8.cr@gmail.com>
2024-05-27 19:12:44 -0700

    query meets activity requirements, my code

--- 3_queries_group-by/6_low_completion_time.sql
+++ 3_queries_group-by/6_low_completion_time.sql
@@ -1,8 +1,8 @@
-SELECT students.name, AVG(assignment_submissions.duration) AS average_completion_time
+SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_completion_time, AVG(assignments.duration) AS average_estimated_time
 FROM assignment_submissions
 JOIN students ON students.id = assignment_submissions.student_id
-
-
+JOIN assignments ON assignments.id = assignment_submissions.assignment_id
 WHERE students.end_date IS NULL
 GROUP BY students.id
+HAVING AVG(assignments.duration) > AVG(assignment_submissions.duration)
 ORDER BY average_completion_time;
