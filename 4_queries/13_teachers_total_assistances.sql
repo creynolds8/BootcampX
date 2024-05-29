4d37a360569a16011e836be66f3d412b0f8b05d4
creynolds8 <cameron8.cr@gmail.com>
2024-05-28 17:02:32 -0700

    remove distinct selecting as unneeded when grouping by teacher name

--- 4_queries/13_teachers_total_assistances.sql
+++ 4_queries/13_teachers_total_assistances.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.id) AS total_assistances
+SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.id) AS total_assistances
 FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON students.id = assistance_requests.student_id
