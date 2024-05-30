4457cb671df7653e6598a6e9873410a5e35b2269
creynolds8 <cameron8.cr@gmail.com>
2024-05-29 22:09:52 -0700

    change input path to stop sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,21 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const cohortName = process.argv[2];
+
+const values = [`%${cohortName}%`];
+
+const queryString = `
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;
+`;
 pool
-  .query(
-  `
-  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-  FROM teachers
-  JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
-  JOIN students ON students.id = assistance_requests.student_id
-  JOIN cohorts ON cohorts.id = students.cohort_id
-  WHERE cohorts.name = '${process.argv[2]}'
-  ORDER BY teachers.name;
-  `
-)
+  .query(queryString, values)
 .then((res) => {
   console.log('connected');
   res.rows.forEach((row) => {
