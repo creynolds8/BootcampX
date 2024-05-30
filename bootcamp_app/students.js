26404e91b20a04864ea03606bfa31bd7f45f62f8
creynolds8 <cameron8.cr@gmail.com>
2024-05-29 22:13:51 -0700

    change input path to stop sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,20 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit]
+
+const queryString = `
   SELECT cohorts.name AS cohort, students.name AS name, students.id AS student_id
   FROM cohorts
   JOIN students ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
   `
-  )
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
