4c5698a2d0aec9ec87b04639fb2b1f1936c9cddd
creynolds8 <cameron8.cr@gmail.com>
2024-05-29 19:28:46 -0700

    copy in query from previous assignment and alter to take in CL argument

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,3 +6,25 @@ const pool = new Pool({
   host: "localhost",
   database: "bootcampx",
 });
+
+pool
+  .query(
+  `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name = '${process.argv[2]}'
+  ORDER BY teachers.name;
+  `
+)
+.then((res) => {
+  console.log('connected');
+  res.rows.forEach((teacher) => {
+    console.log(
+      `${teacher.cohort}: ${teacher.teacher}`
+    );
+  })
+})
+.catch((err) => console.error("query error", err.stack));
